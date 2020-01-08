9a61d7eaff8621303f9a3467035516df97c01197
circleci_ci <ci.circleci.com>
2020-01-08 19:14:58 +0800

    test

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -1,4 +1,4 @@
-git config --global user.email "ci.circleci.com"
+git config --global user.email "circleci@example.com"
 git config --global user.name "circleci_ci"
 git add .
 git commit -m 'circleci auto build commit'
