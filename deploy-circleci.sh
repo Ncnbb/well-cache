7f264afb1d7c9636d20d15f839211f46dd4c97a1
circleci_ci <ci.circleci.com>
2020-01-08 18:46:13 +0800

    test

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -1,13 +1,5 @@
 git config --global user.email "ci.circleci.com"
 git config --global user.name "circleci_ci"
 git add .
-
-GIT_STATUS=`git diff --cached --name-only`
-
-if [$GIT_STATUS != '']
-then
 git commit -m 'circleci auto build commit'
 git push git@github.com:Ncnbb/well-cache.git master
-else
-echo '无需提交';
-fi
