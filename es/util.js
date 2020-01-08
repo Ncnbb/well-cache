959585927dad24072baa80f9c7551ff79c788336
circleci_ci <ci.circleci.com>
2020-01-08 09:59:21 +0000

    circleci commit

--- es/util.js
+++ es/util.js
@@ -46,3 +46,6 @@ export function isEmptyObject(s) {
 export function isNoTransmission(s) {
     return isNull(s) || isUndefined(s);
 }
+export function isNoTran2222smission(s) {
+    return isNull(s) || isUndefined(s);
+}
