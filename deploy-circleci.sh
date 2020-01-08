868507a33f9f699db025fff299ac960e9f6e2ef8
circleci_ci <ci.circleci.com>
2020-01-08 18:28:28 +0800

    test

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -1,5 +1,8 @@
-git config --global user.email "ci.circleci.com"
-git config --global user.name "circleci_ci"
-git add .
-git commit -m 'circleci auto build commit'
-git push git@github.com:Ncnbb/well-cache.git master
+# git config --global user.email "ci.circleci.com"
+# git config --global user.name "circleci_ci"
+# git add .
+# git commit -m 'circleci auto build commit'
+# git push git@github.com:Ncnbb/well-cache.git master
+
+GIT_STATUS=`git status`
+echo ${GIT_STATUS}
