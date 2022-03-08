a267820d9279eb07a4675559356858e879aadc79
lin_shuan <linshuan@zuzuche.com>
2022-03-08 10:48:56 +0800

    修复IDB文件在webpack5解析需要polyfill

--- es/IDB.js
+++ es/IDB.js
@@ -34,7 +34,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-import { isUndefined, isNull, isObject, isFunction } from 'util';
+import { isUndefined, isNull, isObject, isFunction } from './util';
 function openDB(storeName) {
     var _this = this;
     return new Promise(function (resolve) {
